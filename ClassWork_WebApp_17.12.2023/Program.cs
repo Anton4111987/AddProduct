a57691e008536237c1e740abb4bd68ab0faa056d
Anton <laant@mail.ru>
2024-01-29 21:48:17 +0500

    С Serilog

--- ClassWork_WebApp_17.12.2023/Program.cs
+++ ClassWork_WebApp_17.12.2023/Program.cs
@@ -4,47 +4,57 @@ using Microsoft.AspNetCore.Components.Web;
 using ClassWork_WebApp_17._12._2023.Models;
 using Microsoft.EntityFrameworkCore;
 using ClassWork_WebApp_17._12._2023.Services;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
-// Add services to the container.
 
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-
-builder.Services.AddOptions<SmtpConfig>()
-	.BindConfiguration("SmtpConfig")
-	.ValidateDataAnnotations()
-	.ValidateOnStart();
-builder.Services.AddSingleton<IEmailSender, SmtpEmailSender>();
-
-builder.Services.AddScoped<IProductRepository, InDbSQLiteCatalog>();             //      InMemoryCatalog>();                            
-builder.Services.AddSingleton<INowTime, NowTimeInUTC>();
-builder.Services.AddDbContext<AppDbContext>(options =>
+Log.Logger = new LoggerConfiguration()
+	.WriteTo.Console()
+	.CreateLogger();
+try
 {
-	options.UseSqlite(builder.Configuration.GetConnectionString("AppDb"));
-});
+	Log.Information("Starting web application");
+	var builder = WebApplication.CreateBuilder(args);
+	builder.Services.AddRazorPages();
+	builder.Services.AddServerSideBlazor();
+	//var builder = WebApplication.CreateBuilder(args);
+	builder.Host.UseSerilog(); // <-- Add this line
+	builder.Services.AddOptions<SmtpConfig>()
+   .BindConfiguration("SmtpConfig");
+	builder.Services.AddSingleton<IEmailSender, SmtpEmailSender>();
+	builder.Services.AddScoped<IProductRepository, InDbSQLiteCatalog>();             //      InMemoryCatalog>();                            
+	builder.Services.AddSingleton<INowTime, NowTimeInUTC>();
+	builder.Services.AddDbContext<AppDbContext>(options =>
+	{
+		options.UseSqlite(builder.Configuration.GetConnectionString("AppDb"));
+	});
+	builder.Services.AddTransient<ProductService>();
+	var app = builder.Build();
+	// Configure the HTTP request pipeline.
+	if (!app.Environment.IsDevelopment())
+	{
+		app.UseExceptionHandler("/Error");
+		// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+		app.UseHsts();
+	}
 
+	app.UseHttpsRedirection();
 
+	app.UseStaticFiles();
 
-builder.Services.AddTransient<ProductService>();
+	app.UseRouting();
 
-var app = builder.Build();
+	app.MapBlazorHub();
+	app.MapFallbackToPage("/_Host");
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+	app.Run();
+}
+catch (Exception ex)
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+	Log.CloseAndFlush();
 }
 
-app.UseHttpsRedirection();
 
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
-
-app.Run();
---
+++ ClassWork_WebApp_17.12.2023/Services/SmtpEmailSenderDecorator.cs
@@ -0,0 +1,50 @@
+﻿using ClassWork_WebApp_17._12._2023.Models;
+using MailKit;
+using Microsoft.Extensions.Options;
+using Microsoft.VisualBasic.FileIO;
+
+namespace ClassWork_WebApp_17._12._2023.Services
+{
+	public class SmtpEmailSenderDecorator: SmtpEmailSender
+	{
+		
+		protected SmtpEmailSender smtpEmailSender;
+		public new readonly SmtpConfig _smtpConfig;
+		public SmtpEmailSenderDecorator(SmtpEmailSender smtpEmailSender, IOptions<SmtpConfig> opt) : base(opt)
+        {
+			_smtpConfig = opt.Value;
+			this.smtpEmailSender = smtpEmailSender;
+			
+
+		}
+		
+		public async void Send()
+		{
+			/*try
+			{
+				
+				await smtpEmailSender.sendMessage.SendEmail(sendEmailDataModel);
+				dispatchReport = $"Сообщение '{sendEmailDataModel.Subject}' успешно отправлено";
+				Logger.LogInformation("Отправка имейла на адрес {Email} успешно выполнена", sendEmailDataModel.Email);
+				sendEmailDataModel = new();
+			}
+			catch (Exception e) when (_attemptToSend < 3
+								&& e is ServiceNotAuthenticatedException
+								or ServiceNotConnectedException)
+			{
+				Logger.LogWarning(e, "Ошибка отправки емейла на адрес {Email}. Делаем еще 3 попытки", sendEmailDataModel.Email);
+				await Send();
+			}
+			catch (Exception e)
+			{
+				Logger.LogCritical(e, "Ошибка отправки емейла на адрес {Email}. Ошибка: {Error}", sendEmailDataModel.Email, e.Message);
+				dispatchReport = "Ошибка отправки письма ";
+			}*/
+		}
+
+
+
+
+
+    }
+}
